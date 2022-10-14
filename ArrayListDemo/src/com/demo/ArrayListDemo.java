ce36bed9731a2d1f9671a3f4d9098c6bee84c883
manepooja31github <manepooja3129@gmail.com>
2022-10-14 22:57:03 +0530

    First Commit

---
+++ ArrayListDemo/src/com/demo/ArrayListDemo.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list=Arrays.asList("Pooja","Nihal","Sujit","Swara");
+        System.out.println(list);
+	}
+
+}
